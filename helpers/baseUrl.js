e65826b5b3ea37fb41a512f475e304d2232c292e
Bashanta Upreti <ubashanta5@gmail.com>
2021-08-27 02:57:06 +0545

    first

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://bpazesclientchallenge.vercel.app": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://bpazes-client-challenge.vercel.app": "http://localhost:3000"
 export default baseUrl
